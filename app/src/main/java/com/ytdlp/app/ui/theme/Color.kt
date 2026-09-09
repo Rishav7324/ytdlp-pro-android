a3bc0867670db6212142edfb0d97bbae4e71493e
RISHAV RAJ <RISHAVRAJ74883@GMAIL.COM>
2026-09-10 04:24:11 +0530

    ui: tune NovaFetch liquid glass palette

--- app/src/main/java/com/ytdlp/app/ui/theme/Color.kt
+++ app/src/main/java/com/ytdlp/app/ui/theme/Color.kt
@@ -2,35 +2,30 @@ package com.ytdlp.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// NovaFetch brand palette: soft aqua + clean white.
+// NovaFetch liquid-glass palette.
 val NovaAqua = Color(0xFFB1E5E6)
 val NovaAquaSoft = Color(0xFFCCFBFA)
 val NovaAquaDeep = Color(0xFF5C9FA2)
 val NovaInk = Color(0xFF173638)
-
 val NovaPrimary = NovaAquaDeep
 val NovaPrimaryDark = Color(0xFF77BFC2)
 val NovaSecondary = NovaAquaSoft
 val NovaTertiary = NovaAqua
-
 val AccentGreen = Color(0xFF2E9B68)
 val AccentRed = Color(0xFFD95C62)
 val AccentOrange = Color(0xFFD58A35)
-
 val BackgroundDark = Color(0xFF0B1516)
 val SurfaceDark = Color(0xFF122021)
 val SurfaceVariantDark = Color(0xFF1A2B2C)
 val CardBorderDark = Color(0xFF2B4546)
 val TextPrimaryDark = Color(0xFFF8FFFF)
 val TextSecondaryDark = Color(0xFFB8CCCC)
-
-val BackgroundLight = Color(0xFFF8FEFE)
+val BackgroundLight = Color(0xFFF3FAFA)
 val SurfaceLight = Color.White
-val SurfaceVariantLight = Color(0xFFECF8F8)
-val CardBorderLight = Color(0xFFD5EDEE)
+val SurfaceVariantLight = Color(0xFFEAF7F7)
+val CardBorderLight = Color(0xFFD0EAEA)
 val TextPrimaryLight = Color(0xFF173638)
 val TextSecondaryLight = Color(0xFF617B7C)
-
 val YouTubeRed = Color(0xFFFF0033)
 val InstagramPink = Color(0xFFE1306C)
 val TikTokCyan = Color(0xFF00F2FE)
